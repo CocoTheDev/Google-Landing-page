aa0b3543fec2da2872367d6d962078e8230f9edf
Jonathan <jonathan.lutz@gmx.fr>
2020-09-03 20:19:12 +0200

    sidebar js working, but need smoother animation

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-let viewport = 568;
-let elem = 48;
+const sidebar_back = document.getElementById('modal-back');
+const sidebar = document.getElementById('sidebar-container');
+const hamburger = document.getElementById('hamburger');
 
-let result = elem/(viewport/100)
+hamburger.addEventListener('click', toggle, false);
+sidebar_back.addEventListener('click', toggle, false);
 
+function toggle() {
+    console.log('hello');
+    sidebar_back.classList.toggle('show');
+    sidebar.classList.toggle('show');
 
-
-console.log(`${elem}px de ${viewport}px : ${result}%`);
+    
+    // sidebar_back.style.right = '0';
+    // sidebar.style.right = 'null';
+    // sidebar.style.left = '0';
+}
